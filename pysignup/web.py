a63c32b497c65bf7897e374a52e42985a5188eac
Michael Schurter <m@schmichael.com>
2013-10-03 21:19:09 -0700

    Hack in serving static files from flask

--- pysignup/web.py
+++ pysignup/web.py
@@ -1,7 +1,41 @@
-from flask import Flask
+import os
+
+from flask import Flask, send_from_directory
+
+
 app = Flask('pysignup')
 
 BASE_URL = '/api/'
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+@app.route("/assets/<path:filename>")
+def get_assets(filename):
+    path = os.path.join(BASE_DIR, 'assets')
+    return send_from_directory(path, filename)
+
+
+@app.route("/404.html")
+def get_404():
+    return send_from_directory(BASE_DIR, '404.html')
+
+
+@app.route("/")
+@app.route("/index.html")
+def get_index():
+    return send_from_directory(BASE_DIR, 'index.html')
+
+
+@app.route("/schedule")
+@app.route("/schedule.html")
+def get_schedule():
+    return send_from_directory(BASE_DIR, 'schedule.html')
+
+
+@app.route("/event")
+@app.route("/event.html")
+def get_event():
+    return send_from_directory(BASE_DIR, 'event.html')
 
 
 def discover_views():
